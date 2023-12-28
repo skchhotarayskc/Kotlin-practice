4cf0973671e974d5ed2c8e896e52110a692fd994
Sushant <skchhotarayskc@gmail.com>
2023-12-28 19:29:11 +0530

    Third Commit

--- classes.kt
+++ classes.kt
@@ -5,14 +5,14 @@ class Contact(val id: Int, var email: String = "examples@gmail.com") {
     }
 }
 
-class Person {
-    var name: String = ""
-    var age: Int = 0
+// class Person {
+//     var name: String = ""
+//     var age: Int = 0
 
-    fun speak() {
-        println("Hello my name is $name and i am $age years old")
-    }
-}
+//     fun speak() {
+//         println("Hello my name is $name and i am $age years old")
+//     }
+// }
 
 class Student {
     var name: String = ""
@@ -34,11 +34,45 @@ class Student {
     }
 }
 
+// primary constructor and initializer blocks
+class Person(fName: String, personAge: Int){
+    val firstName: String = fName
+    var age: Int = personAge
+
+    // initializer block
+    // when an instance of the class is declared then this block will be executed
+    init{
+        println("First Name = $firstName")
+        println("Age = $age")
+    }
+}
+
+open class Log {
+    var data: String = ""
+    var numberOfData = 0
+    constructor(_data: String) {
+
+    }
+    constructor(_data: String, _numberOfData: Int) {
+        data = _data
+        numberOfData = _numberOfData
+        println("$data: $numberOfData times")
+    }
+}
+
+class AuthLog: Log {
+    constructor(_data: String): this("From AuthLog -> " + _data, 10) {
+    }
+
+    constructor(_data: String, _numberOfData: Int): super(_data, _numberOfData) {
+    }
+}
+
 fun main() {
-    val person1 = Person()
-    person1.name = "Sushant"
-    person1.age = 22
-    person1.speak()
+    // val person1 = Person()
+    // person1.name = "Sushant"
+    // person1.age = 22
+    // person1.speak()
 
     val contact1 = Contact(101, "skc@gmail.com")
     contact1.category = "cook"
@@ -54,4 +88,8 @@ fun main() {
     student2.showDetails()
 
     // println(student2.toString())
+
+    val person1 = Person("Sushant", 22)
+
+    val p1 = AuthLog("Bad Password")
 }
